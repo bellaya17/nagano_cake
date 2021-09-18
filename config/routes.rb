5df89cce3ad35870619e9d8ba2262540f4cfb7ec
bellaya17 <belltreeayaka@gmail.com>
2021-09-18 07:52:47 +0000

    [Start]注文履歴一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :admins
 
   resources :genres
-  
+
   namespace :admin do
   resources :items
+  resources :customers
+  resources :orders
+  resources :order_details
   end
 
 end
