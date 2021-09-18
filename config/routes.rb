a43404d6b3f75425ec8e04b317e1a1bd65a15d53
bellaya17 <belltreeayaka@gmail.com>
2021-09-18 12:07:40 +0000

    [Fix]レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
-
   resources :genres
 
+
   namespace :admin do
+  root to: 'homes#top'
   resources :items
   resources :customers
   resources :orders
